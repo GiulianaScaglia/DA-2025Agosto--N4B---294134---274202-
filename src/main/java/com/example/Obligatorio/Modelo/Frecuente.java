ffc376dbf694327c7111838dd3f4c161509543eb
GiulianaScaglia <gs274202@fi365.ort.edu.uy>
2025-10-31 21:14:49 -0300

    Frecuente hereda de bonificacion, falta implementar validar

---
+++ src/main/java/com/example/Obligatorio/Modelo/Frecuente.java
@@ -0,0 +1,9 @@
+public class Frecuente extends Bonificacion {
+    public Frecuente() {
+        super("Frecuente");
+    }   
+
+    public void validar() {
+        // Implementación específica para Frecuente
+    }
+}
