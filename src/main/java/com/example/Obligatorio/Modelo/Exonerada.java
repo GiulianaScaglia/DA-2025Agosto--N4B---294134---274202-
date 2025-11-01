25c23ad55ab76bfedf4e6b0a6930a05e345e3a40
GiulianaScaglia <gs274202@fi365.ort.edu.uy>
2025-10-31 21:14:35 -0300

    exonerada hereda abstracta, falta implementar validar

---
+++ src/main/java/com/example/Obligatorio/Modelo/Exonerada.java
@@ -0,0 +1,10 @@
+public class Exonerada extends Bonificacion {
+    public Exonerada() {
+        super("Exonerada");
+    }       
+
+    public void validar() {
+        // Implementación específica para Exonerada
+    }
+
+}
